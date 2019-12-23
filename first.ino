a82e3153d4c88e44c2272bcfb9f52ccbaa92c51a
m17saitou <m17saitou@gmail.com>
2019-12-24 05:16:58 +0900

    Add:ソースファイルを編集

--- first.ino
+++ first.ino
@@ -1,9 +1,19 @@
 #include "saitoRobo.hpp"
 void setup(){
-    pinMode(1,OUTPUT);
+    pinMode(2,OUTPUT);
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(8,INPUT);
+    pinMode(9,INPUT);
+    pinMode(11,OUTPUT);
+    pinMode(12,OUTPUT);
 }
 
+
+
 void loop(){
-    SaitoRobo drive = SaitoRobo(1);
-    drive.pinOut();
+	SaitoRobo Robo = SaitoRobo(2,3,4,5,11);
+	digitalWrite(2,1);
+	Robo.turnLeft();
 }
--- saitoRobo.hpp
+++ saitoRobo.hpp
@@ -4,14 +4,25 @@
 
 class SaitoRobo{
 public:
-    SaitoRobo(int a){
-        num=a;
+    SaitoRobo(int rF,int rR,int lF,int lR,int B){
+        rMF = rF;
+        rMR = rR;
+        lMF = lF;
+        lMR = lR;
+        Ball = B;
     }
-    void pinOut(){
-        digitalWrite(SaitoRobo::num,HIGH);
+    void turnLeft(){
+        digitalWrite(lMF,1);
+        digitalWrite(rMR,1);
+        delay(1000);
+        digitalWrite(lMF,0);
+        digitalWrite(rMR,0);
     }
-private:
-    int num;
+    int rMF,
+        rMR,
+        lMF,
+        lMR,
+        Ball;
 };
 
 #endif // SAITOROBO_HPP
