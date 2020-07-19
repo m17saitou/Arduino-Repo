d6335e3da5ffac448717fc7038e89001055f801d
m17saitou <m17saitou@gmail.com>
2020-07-19 12:02:37 +0900

    Bug Fix

--- LCD.ino
+++ LCD.ino
@@ -3,7 +3,7 @@
 //PIN 2 = Ratch Clock   =Yellow
 #define HIGH 1
 #define LOW 0
-
+void initLCD();
 void setup(){
     pinMode(6,OUTPUT);
     pinMode(4,OUTPUT);
@@ -11,15 +11,27 @@ void setup(){
 }
 
 void loop(){
-    digitalWrite(2,HIGH);
+    initLCD();
     char str[6]={"hello"};
     for(int i:str){
-        digitalWrite(2,HIGH);
+        digitalWrite(2,LOW);
         for(int8_t bit=0;bit<8;bit++){
-            digitalWrite(4,HIGH);
-            digitalWrite(6,str[i]&0b00000001<<bit);
             digitalWrite(4,LOW);
+            digitalWrite(6,str[i]&0b00000001<<bit);
+            digitalWrite(4,HIGH);
         }
         digitalWrite(2,HIGH);
     }
+}
+
+void initLCD(){
+    digitalWrite(2,LOW);
+    for(int8_t i=0;i<8;i++){
+        digitalWrite(4,LOW);
+        digitalWrite(6,0b00111100&0b00000001<<i);
+        digitalWrite(4,HIGH);
+    }
+    digitalWrite(2,HIGH);
+    digitalWrite(2,LOW);
+    delay(15);
 }
