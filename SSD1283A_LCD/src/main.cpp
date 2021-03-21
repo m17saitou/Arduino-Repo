3508060f72994b0b06fad39c7b15aeb5cea3987d
kasagon <m17saitou@gmail.com>
2021-03-21 12:47:08 +0900

    LCD上で温湿度を表示

--- SSD1283A_LCD/src/main.cpp
+++ SSD1283A_LCD/src/main.cpp
@@ -29,19 +29,46 @@ LCDWIKI_SPI mylcd (MODEL,CS,CD, MISO, MOSI, RST, SCK, LED);
 #define YELLOW 0xFFE0
 #define WHITE 0xFFFF
 
+void printToLCD(float tempHumidArr[2]);
+void getTempHumid(float tempHumidArr[2]);
+
 void setup(){
     mylcd.Init_LCD ();
     mylcd.Fill_Screen (0x00FFFF);
+    mylcd.Set_Text_Mode (0);
+    Wire.begin();
+    delay(100);
 }
 
 void loop (){
-    mylcd.Set_Text_Mode (0);
-
-    mylcd.Fill_Screen (0x00FFFF);
-    mylcd.Set_Text_colour (RED);
-    mylcd.Set_Text_Back_colour (CYAN);
+    mylcd.Set_Text_colour (BLACK);
+    mylcd.Set_Text_Back_colour(WHITE);
     mylcd.Set_Text_Size (2);
-    mylcd.Print_String ("Hello Kasagone", 0, 0);
+    mylcd.Print_String ("Temperature", 2, 2);
+    mylcd.Print_String("Humidity",2,44);
+    float tempHum[2];
+    getTempHumid(tempHum);
+    printToLCD(tempHum);
+    delay(1500);
+}
 
-    delay (2000);
+void printToLCD(float tempHumidArr[2]){
+    mylcd.Set_Text_Size(3);
+    mylcd.Set_Text_colour(GREEN);
+    mylcd.Print_Number_Float(tempHumidArr[0],2,10,18,'.',5,' ');
+    mylcd.Set_Text_colour(MAGENTA);
+    mylcd.Print_Number_Float(tempHumidArr[1],2,10,62,'.',5,' ');
+}
+
+void getTempHumid(float arr[2]){
+    unsigned char sensorData[7];
+    Wire.beginTransmission(0x44);
+    Wire.write(0x2C);
+    Wire.write(0x06);
+    Wire.endTransmission();
+    delay(50);
+    Wire.requestFrom(0x44,6);
+    for(unsigned char i = 0; Wire.available() > 0; i++)sensorData[i] = Wire.read();
+    arr[0] = ((((sensorData[0] * 256.0) + sensorData[1]) * 175) / 65535.0) - 45;//Temperature
+    arr[1] = ((((sensorData[3] * 256.0) + sensorData[4]) * 100) / 65535.0);
 }
